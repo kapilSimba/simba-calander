24ce4ff2669f36cdcdd241624af329397305f1e9
harmeet kaur <gurpreetsra@Gurpreets-MacBook-Pro.local>
2022-07-30 16:03:20 +0530

    calander add

--- pages/calander/index.jsx
+++ pages/calander/index.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, {useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-export default function Calander() {
+
+
+export default function Calander({column}) {
   const [value, onChange] = useState(new Date());
 
+
   return (
-    <div className="">
+    <div className=" flex h-[60vh] bg-blue-800 mr-8">
       <Calendar onChange={onChange} value={value} />
+    
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import Calander from "./calander";
+import Tables from "./table";
 
-export default function Home() {
+export default function Home({columns}) {
   return (
-    <div className="bg-red-400 flex justify-center py-6 h-[100vh]">
+    <div className="bg-red-400 flex justify-start gap-8 py-6 h-[100vh] flex">
+    
       <Calander />
+      <Tables/>
     </div>
   );
 }
